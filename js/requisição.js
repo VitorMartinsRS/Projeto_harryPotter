d3522abdccd846e5ab2dbf46de47fae7532e3bfe
vitormartins <vitorrsilveriomartins@gmail.com>
2025-06-09 11:30:56 -0300

    WW

--- js/requisição.js
+++ js/requisição.js
@@ -1,4 +1,5 @@
 const url = 'https://potterhead-api.vercel.app/api/movies'
+const filmes = document.querySelector('.card-container')
 const titulosOriginais = Array.from(document.querySelectorAll('.titulo')).map(t => t.textContent)
 
 // aplicação da API
@@ -6,41 +7,41 @@ function testeApi(filme, i){
   const titulo = titulosOriginais[i]
   const favoritos = JSON.parse(localStorage.getItem('favoritos')) || []
   const jaFavorito = favoritos.some(fav => fav.titulo === titulo)
-  
+
   return `
-  <li>    
-  <div class="card">
-  <p>
-  <button class="estrela">&#9733;</button></p>
-  <img src="${filme.poster}" alt="">
-  <h2>${filme.serial} - ${titulosOriginais[i]}</h2>
-  </div>
-  </li>
-  `
+        <li>    
+        <div class="card">
+        <p>
+        <button class="estrela">&#9733;</button></p>
+        <img src="${filme.poster}" alt="">
+        <h2>${filme.serial} - ${titulosOriginais[i]}</h2>
+        </div>
+        </li>
+        `
 }
 // aplicação da API
 
 function salvar(filmeData){
   const estrelas = document.querySelectorAll('.estrela')
   estrelas.forEach((estrela, index) => {
-    estrela.addEventListener('click', () => {
-      
-      let titulo = estrela[index]
-      let favoritos = JSON.parse(localStorage.getItem('favoritos')) || []
-      const jaFavorito = favoritos.some(f => f.titulo === titulo)
-      
-      if (jaFavorito) {
-        estrela.classList.remove('favorito')
-        estrela.style.color = 'white'
-        favoritos = favoritos.filter(f => f.titulo !== titulo)
-      }else{
-        estrela.classList.add('favorito')
-        estrela.style.color = 'yellow'
-        let filme = { ...filmeData[index], titulo:titulo }
-        favoritos.push(filme) 
-      }
-      localStorage.setItem('favoritos', JSON.stringify(favoritos))
-      
+  estrela.addEventListener('click', () => {
+    
+    let titulo = estrela[index]
+    let favoritos = JSON.parse(localStorage.getItem('favoritos')) || []
+    const jaFavorito = favoritos.some(f => f.titulo === titulo)
+
+    if (jaFavorito) {
+      estrela.classList.remove('favorito')
+      estrela.style.color = 'white'
+      favoritos = favoritos.filter(f => f.titulo !== titulo)
+    }else{
+      estrela.classList.add('favorito')
+      estrela.style.color = 'yellow'
+      let filme = { ...filmeData[index], titulo:titulo }
+      favoritos.push(filme) 
+    }
+    localStorage.setItem('favoritos', JSON.stringify(favoritos))
+    
   })
 })}
 //tentativa frustrada de tentar aplicar o status de "favorito"
@@ -49,13 +50,8 @@ function salvar(filmeData){
 fetch(url)
 .then(response => response.json())
 .then(jsonresponse => {
-  const filmes = document.querySelector('.card-container')
-// Geração dinâmica com base na API e nos títulos da DOM
-    filmes.innerHTML = jsonresponse.map((filme, i) => {
-      return testeApi(filme, i, titulosOriginais[i])
-    }).join("")
-
-    // Depois que tudo foi renderizado
-    salvar(jsonresponse, titulosOriginais)
-  })
-  .catch((error) => console.log(error))
+  filmes.innerHTML = jsonresponse.map((filme, i) => testeApi(filme, i)).join("")
+  salvar(jsonresponse)
+})
+.catch((error) => console.log(error))
+//importação do fetch
