cf0e86187ee71313230c5607061d332e81d2fba2
Jônatas Monteiro <jonatasmonteiro2006@gmail.com>
2025-09-12 21:55:32 -0300

    Pagina de filmes header e footer
    ta ok

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-$(document).ready(function () {
-    $('#mobile_btn').on('click', function () {
-        $('#mobile_menu').toggleClass('active');
-        const icon = $(this).find('i');
-        icon.toggleClass('fa-bars fa-xmark');
-    });
-  });
+const mobileBtn = document.getElementById('mobile_btn');
+const mobileMenu = document.getElementById('mobile_menu');
+
+mobileBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+});
